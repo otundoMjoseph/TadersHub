09c7ea7a6d8adf4fdad2d796463eb138b5bcac30
JosephMainaOtundo <otundomainajoseph@gmail.com>
2024-07-09 22:48:09 +0300

    added app.js && navbar.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,33 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import ItemList from './components/ItemList';
+import ItemDetail from './components/ItemDetail';
+import ItemForm from './components/ItemForm';
+import OrderForm from './components/OrderForm';
+import OrderList from './components/OrderList';
+import UserDetail from './components/UserDetail';
+import CategoryList from './components/CategoryList';
+import FeedbackForm from './components/FeedbackForm';
+import FeedbackList from './components/FeedbackList';
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/items" component={ItemList} />
+        <Route path="/item/:id" component={ItemDetail} />
+        <Route path="/create-item" component={ItemForm} />
+        <Route path="/place-order" component={OrderForm} />
+        <Route path="/orders" component={OrderList} />
+        <Route path="/user/:id" component={UserDetail} />
+        <Route path="/categories" component={CategoryList} />
+        <Route path="/feedback/:itemId" component={FeedbackList} />
+        <Route path="/leave-feedback" component={FeedbackForm} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/items">Items</Link></li>
+        <li><Link to="/create-item">Create Item</Link></li>
+        <li><Link to="/place-order">Place Order</Link></li>
+        <li><Link to="/orders">Orders</Link></li>
+        <li><Link to="/categories">Categories</Link></li>
+        <li><Link to="/leave-feedback">Leave Feedback</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
