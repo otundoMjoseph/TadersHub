a600cf02bcc8999143fa8684d3ae464498ccba69
ADNAN-VICTOR <162279297+ADNAN-VICTOR@users.noreply.github.com>
2024-07-14 14:29:17 +0300

    Update NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -9,10 +9,10 @@ function NavBar() {
          <li><Link to="/profile">Profile</Link></li>
         <li><Link to="/items">Items</Link></li>
         <li><Link to="/create-item">Create Item</Link></li>
-        <li><Link to="/place-order">Place Order</Link></li>
+        {/* <li><Link to="/place-order">Place Order</Link></li> */}
         <li><Link to="/orders">Orders</Link></li>
         <li><Link to="/categories">Categories</Link></li>
-        <li><Link to="/search">Search</Link></li>
+        {/* <li><Link to="/search">Search</Link></li> */}
         <li><Link to="/leave-feedback">Leave Feedback</Link></li>
       </ul>
     </nav>
